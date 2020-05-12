cc6ae6590abc449acb78a2f5ae50777601045ef4
robsel118 <robertleeseligmann@gmail.com>
2020-05-12 17:57:29 +0300

    added first test

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,12 @@
 import * as mongoose from "mongoose";
+import * as bcrypt from 'bcrypt';
+import * as Joi from 'joi';
 
-
-interface IUser extends mongoose.Document {
+export interface IUser extends mongoose.Document {
   name: string;
   email: string;
   hash: string;
+  setHash: (string)=> void
 }
 
 export const UserSchema = new mongoose.Schema(
@@ -32,6 +34,23 @@ export const UserSchema = new mongoose.Schema(
   }
 );
 
+UserSchema.methods.setHash = function (password: String) {
+  var user = this;
 
-const User = mongoose.model<IUser>('User', UserSchema);
+  bcrypt.hash(password, 10, function (err, hash) {
+    user.hash = hash;
+  });
+};
+
+UserSchema.methods.validatePayload = function (obj: Object) {
+  var schema = Joi.object({
+    name: Joi.string().min(4).required(),
+    email: Joi.string().email().required,
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{6,30}$")).required,
+  });
+
+  return schema.validate(obj);
+};
+
+const User = mongoose.model<IUser>("User", UserSchema);
 export default User;
