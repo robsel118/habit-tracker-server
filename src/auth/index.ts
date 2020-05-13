994b295a6ec583a540a469a5662c883896048f3d
robsel118 <robertleeseligmann@gmail.com>
2020-05-13 12:21:48 +0300

    switching branch

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -6,3 +6,55 @@ import * as compose from "koa-compose";
 
 import jwtStrategy from "./strategies/jwt";
 import emailStrategy from "./strategies/email";
+
+passport.use("jwt", jwtStrategy);
+passport.use("email", emailStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  (async () => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  })();
+});
+
+export default function auth() {
+  return compose([passport.initialize()]);
+}
+
+export function isAuthenticated() {
+  return passport.authenticate("jwt");
+}
+
+export function authEmail() {
+  return passport.authenticate("email");
+}
+
+export function generateToken() {
+  return async (ctx) => {
+    const { user } = ctx.passport;
+    if (user === false) {
+      ctx.status = 401;
+    } else {
+      const jwtToken = jwt.sign({ id: user }, process.env.PASSPORT_SECRET, {
+        expiresIn: 60 * 60,
+      });
+      const token = `JWT ${jwtToken}`;
+
+      const currentUser = await User.findOne({ _id: user });
+
+      ctx.status = 200;
+      ctx.body = {
+        token,
+        user: currentUser,
+      };
+    }
+  };
+}
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import User from "./models/User";
+import User from "models/User";
 import { assert, expect } from "chai";
 
 describe("User model", () => {
