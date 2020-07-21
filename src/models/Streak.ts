47a939706ef90ec640576e4624dd26ab376f931a
robsel118 <robertleeseligmann@gmail.com>
2020-07-21 13:33:07 +0300

    updated streak model

--- src/models/Streak.ts
+++ src/models/Streak.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import { HabitType } from "./Habit";
+import { differenceInDays } from "date-fns";
 
 export interface StreakType extends mongoose.Document {
   startDate: Date;
   endDate: Date;
+  habit: HabitType;
 }
 
 export const StreakSchema = new mongoose.Schema({
@@ -14,6 +17,10 @@ export const StreakSchema = new mongoose.Schema({
   },
   habit: { type: mongoose.Schema.Types.ObjectId, ref: "Habit" },
 });
+
+StreakSchema.methods.getLength = function () {
+  return differenceInDays(this.start, this.end);
+};
 
 const Streak = mongoose.model<StreakType>("Streak", StreakSchema);
 export default Streak;
