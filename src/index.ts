060afa4c39f266c2e4ae9d6531b83c7cba7fe3c8
robsel118 <robert.seligmann@protonmail.ch>
2020-10-28 18:14:37 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import connectDatabase from "./db";
 const DB_URI = process.env.MONGO_URI || "mongodb://mongodb:27017/habit_tracker";
 const PORT = process.env.PORT || 3000;
 
-// connectDatabase(DB_URI);
+connectDatabase(DB_URI);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
